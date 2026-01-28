2a0615c36752c252a848a68665670cc8aa58c462
Dayana <dayileyvas@gmail.com>
2026-01-27 19:46:11 -0600

    swaggerUI

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const dashboardRouter = require("./routes/dashboard");
 const profilesRouter = require("./routes/workerProfiles");
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
@@ -13,6 +20,18 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+app.set("trust proxy", 1);
+
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 100, // 100 requests per IP per window
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.get("/", (req, res) => {
@@ -21,6 +40,7 @@ app.get("/", (req, res) => {
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/profiles", profilesRouter);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
