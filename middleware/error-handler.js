4ea4e92a64ef15f8ea85bf3e6237ad2dc0f6021a
Dayana <dayileyvas@gmail.com>
2026-02-08 22:43:25 -0600

    Week10 complete: security middleware, error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,26 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
-const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+const { StatusCodes } = require("http-status-codes");
 
-module.exports = errorHandlerMiddleware
+const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong, please try again",
+  };
+
+  //  Mongoose validation errors (required fields, enum, etc.)
+  if (err.name === "ValidationError") {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(", ");
+  }
+
+  //  Duplicate key error (email already exists)
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.CONFLICT; // 409
+    customError.msg = "Email already exists";
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
